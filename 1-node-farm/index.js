27abcb0d555b5f055f9079f955cd31621e95241a
Mareike Heuer <mareike.heuer@hotmail.com>
2023-04-20 12:00:19 +0200

    Reading & writing files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,3 +1,9 @@
 // get access to the file system 
 const fs = require('fs');
 
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textInput);
+
+const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOutput);
+console.log('File written');
