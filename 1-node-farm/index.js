d035e27f31a4f8c86287eca32f9c4cfcf56f38fa
Mareike Heuer <mareike.heuer@hotmail.com>
2023-04-24 00:04:46 +0200

    Using 3rd party modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const http = require("http"); // networking capabilites
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //////////////////////////////
@@ -47,8 +49,12 @@ const templateProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+// console.log(slugify("Fresh Avocados", { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   // const { query, pathname } = url.parse(req.url, true);
