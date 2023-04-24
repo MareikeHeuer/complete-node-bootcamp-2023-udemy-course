8cce8376716fbf383c489828e37e052a0942e5c0
Mareike Heuer <mareike.heuer@hotmail.com>
2023-04-24 12:20:56 +0200

    ASYNC JS - Example of callback hell

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -3,4 +3,16 @@ const superagent = require("superagent");
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body.message);
+      if (err) return console.log(err.message);
+
+      fs.writeFile("dog-img.text", res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log("Random dog image saved to file");
+      });
+    });
 });
