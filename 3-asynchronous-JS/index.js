71bc38fd401579bcb1cccf768572006967064fc4
Mareike Heuer <mareike.heuer@hotmail.com>
2023-04-25 00:12:26 +0200

    ASYNC JS - Returning values from async functions

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -33,6 +33,30 @@ const getDogPic = async () => {
     console.log("Random dog image saved to file");
   } catch (err) {
     console.log(err);
+
+    throw err; // Will mark entire promise as rejected
   }
+  return "2: READY";
 };
-getDogPic();
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Done getting dog pics");
+  } catch (err) {
+    console.log("ERROR");
+  }
+})();
+
+// console.log("1: Will get dog pics");
+// getDogPic()
+//   .then((x) => {
+//     // .then to make sure console log stataments run in order
+//     console.log(x);
+//     console.log("3: Done getting dog pics");
+//   })
+//   .catch((err) => {
+//     console.log("ERROR");
+//   });
